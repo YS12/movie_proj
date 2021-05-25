32825d478e0a1c0092c90ffea86e5c9d2909483b
Y(K)S <ys367@cornell.edu>
2021-05-25 12:14:44 +0900

    root-context의 localhost 수정

--- movie_proj/src/main/java/org/movieprj/controller/HomeController.java
+++ movie_proj/src/main/java/org/movieprj/controller/HomeController.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.movieprj.service.MemberService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -13,25 +14,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@Log4j
+@RequestMapping("/*")
+@AllArgsConstructor
 public class HomeController {
 	
 //	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private MemberService service;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		return "home";
+		return "/home";
 	}
 	
 	@PostMapping(value="/")
 	public String home(@RequestParam("id") String id, @RequestParam("pw") String pw) {
-		
+		log.info("test id : " + id + ", pw : " + pw);
 		return "redirect:/board/list";
 	}
 }
